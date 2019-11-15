b37387d4ecf376128f7a264f983b89a8b8f9c879
neeson <1043358774@qq.com>
2019-11-15 17:21:52 +0800

    K8S

---
+++ k8s/learn/daemon/fluentd-image.sh
@@ -0,0 +1,3 @@
+docker pull mirrorgooglecontainers/fluentd-elasticsearch:v2.2.0
+docker tag mirrorgooglecontainers/fluentd-elasticsearch:v2.2.0 k8s.gcr.io/fluentd-elasticsearch:v2.2.0
+docker rmi mirrorgooglecontainers/fluentd-elasticsearch:v2.2.0
---
+++ zookeeper/image.sh
@@ -0,0 +1,3 @@
+docker pull mirrorgooglecontainers/kubernetes-zookeeper:1.0-3.4.10
+docker tag mirrorgooglecontainers/kubernetes-zookeeper:1.0-3.4.10 k8s.gcr.io/kubernetes-zookeeper:1.0-3.4.10
+docker rmi mirrorgooglecontainers/kubernetes-zookeeper:1.0-3.4.10
