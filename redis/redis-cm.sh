2ad7f1a2bd02499e552f9ce3b626f464c47fbeca
neeson <d1043358774@gmail.com>
2019-11-17 16:25:46 +0800

    k8s

--- redis/redis-cm.sh
+++ redis/redis-cm.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-kubectl create configmap redis-conf --from-file=redis.conf
+
+# --replicas: 每个master有几个slave
+kubectl exec -it redis-app-0 -- redis-cli --cluster create --cluster-replicas 1 $(kubectl get pods -l appCluster=redis-cluster -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
